4578ee1496732d2b50611b68726b83f9784903ae
jaidharosenblatt <jaidha.rosenblatt@gmail.com>
2019-06-09 16:19:42 -0700

    2nd deploy

--- TutorEDU/settings.py
+++ TutorEDU/settings.py
@@ -136,10 +136,10 @@ CORS_ORIGIN_ALLOW_ALL = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# #
-# # STATICFILES_DIRS = [
-# #     os.path.join(BASE_DIR, 'build/static'),
-# # ]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
