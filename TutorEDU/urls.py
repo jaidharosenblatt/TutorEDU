8f702d55bc64c1d62fb05001d9ea78ff96799d47
jaidharosenblatt <jaidha.rosenblatt@gmail.com>
2019-06-10 21:45:57 -0700

    fixed image

--- TutorEDU/urls.py
+++ TutorEDU/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    # path('api-auth/', include('rest_framework.urls')),
     path('api/',include('myApp.api.urls')),
     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
